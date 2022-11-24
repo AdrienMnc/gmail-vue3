c84a7a00f63c6dbfbc693900d2608a2f58912625
Adrien <adrien.moncieu@gmail.com>
2022-11-24 08:58:13 +0100

    vue mastery course

---
+++ src/components/MailView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    email: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped></style>
