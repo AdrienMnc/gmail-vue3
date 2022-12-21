a591d3a0afa6b13ec0915354bce2adc4b1355d49
Adrien <adrien.moncieu@gmail.com>
2022-12-21 08:43:33 +0100

    next

--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="email-display">
     <div class="close-modal" @click="emit('closeModal')">X</div>
+    <div>
+      <button>Archive</button>
+      <button @click="toggleRead">{{ email.read ? "Mark Unread (r)" : "Mark Read (r)" }}</button>
+      <button>Newer</button>
+      <button>Older</button>
+    </div>
     <h2 class="mb-0">
       Subject: <strong>{{ email.subject }}</strong>
     </h2>
@@ -14,9 +20,24 @@
 <script>
 import { format } from "date-fns";
 import marked from "marked";
+import axios from "axios";
+// import useKeydown from "../composables/use-keydown";
 
 export default {
   setup(props, { emit }) {
+    let email = props.email;
+    let toggleRead = () => {
+      email.read = !email.read;
+      axios.put(`http://localhost:3000/emails/${email.id}`, email);
+    };
+
+    // usekeydown([
+    //   {
+    //     key: "r",
+    //     fn: toggleRead
+    //   }
+    // ]);
+
     return {
       format,
       marked,
